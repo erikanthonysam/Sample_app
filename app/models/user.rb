02e19966da10cd0eccd17999e8fcc1b1a71ab053
Erik Sam <erikanthonysam@gmail.com>
2011-03-13 00:31:57 -0800

    Ch7.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,9 +26,14 @@ class User < ActiveRecord::Base
                         
   before_save :encrypt_password
   
-  #Return true if the user's password matches the submitted password.
   def has_password?(submitted_password)
     encrypted_password == encrypt(submitted_password)
+  end
+  
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil if user.nil?
+    return user if user.has_password?(submitted_password)
   end
   
   private
@@ -48,8 +53,7 @@ class User < ActiveRecord::Base
     
     def secure_hash(string)
       Digest::SHA2.hexdigest(string)
-    end
-                
+    end         
                     
   
 end
