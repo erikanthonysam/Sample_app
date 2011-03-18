5b28575292dc3dcc94caeae1e592a2d2935fa6b5
Erik Sam <erikanthonysam@gmail.com>
2011-03-17 20:52:51 -0700

    Ch9.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :sessions, :only => [ :new, :create, :destroy]
+  
   
   match '/signup',    :to => 'users#new'
+  match '/signin',    :to => 'sessions#new'
+  match '/signout',   :to => 'sessions#destroy'
 
   match '/contact',   :to => 'pages#contact'
   match '/about',     :to => 'pages#about'
@@ -13,6 +17,8 @@ SampleApp::Application.routes.draw do
   get "pages/contact"
   get "pages/about"
   get "pages/help"
+  get "sessions/new"
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
