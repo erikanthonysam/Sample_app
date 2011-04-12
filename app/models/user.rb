64f01a5d034d3bfa04545809065d4b604e42f0c1
Erik Sam <erikanthonysam@gmail.com>
2011-04-11 20:55:59 -0700

    Ch11.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,8 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
+  has_many :microposts, :dependent => :destroy
+  
   validates :name,  :presence => true,
                     :length   => { :maximum => 50 }
   validates :email, :presence => true,
