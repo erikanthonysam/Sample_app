3c8efaeced1dfe1960534ebd1b697053408cb218
Erik Sam <erikanthonysam@gmail.com>
2011-04-19 21:01:06 -0700

    Ch12.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,13 @@ class User < ActiveRecord::Base
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
   has_many :microposts, :dependent => :destroy
+  has_many :relationships, :foreign_key => "follower_id",
+                           :dependent => :destroy
+  has_many :following, :through => :relationships, :source => :followed
+  has_many :reverse_relationships,  :foreign_key => "followed_id",
+                                    :class_name => "Relationship",
+                                    :dependent => :destroy
+  has_many :followers, :through => :reverse_relationships, :source => :follower
   
   validates :name,  :presence => true,
                     :length   => { :maximum => 50 }
@@ -46,6 +53,19 @@ class User < ActiveRecord::Base
   def feed
     Micropost.where("user_id = ?", id)
   end
+  
+  def following?(followed)
+    relationships.find_by_followed_id(followed)
+  end
+  
+  def follow!(followed)
+    relationships.create!(:followed_id => followed.id)
+  end
+  
+  def unfollow!(followed)
+    relationships.find_by_followed_id(followed).destroy
+  end
+  
   
   private
   
